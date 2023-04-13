cfb460fd405810d948841b2257f24c99b38ab39b
Thomas Chaplin <tomrchaplin@gmail.com>
2023-04-13 12:22:04 +0100

    Add cone_time truncation to default pipelines

--- grpphati_rs/pipelines.py
+++ grpphati_rs/pipelines.py
@@ -3,12 +3,14 @@ from grpphati.filtrations import ShortestPathFiltration
 from grpphati.optimisations import component_appendage_empty, all_optimisations
 from grpphati.backends import LoPHATBackend
 from grpphati.pipelines.grounded import GrPPH, make_grounded_pipeline
+from grpphati.truncations import cone_time
 
 GrPPH_rs = make_grounded_pipeline(
     ShortestPathFiltration,
     RustRegularPathHomology,
     backend=LoPHATBackend(sparsifier=RustPreferredSparsifier(2)),
     optimisation_strat=component_appendage_empty,
+    truncation_strat=cone_time,
 )
 
 GrPPH_par_wedge_rs = make_grounded_pipeline(
@@ -16,4 +18,5 @@ GrPPH_par_wedge_rs = make_grounded_pipeline(
     RustRegularPathHomology,
     backend=LoPHATBackend(sparsifier=RustPreferredSparsifier(2)),
     optimisation_strat=all_optimisations,
+    truncation_strat=cone_time,
 )
